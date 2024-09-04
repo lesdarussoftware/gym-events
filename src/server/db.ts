0475746d03344e3d0c220d3f5cf5dab5e10007b8
matias ramos <matiasramos261@gmail.com>
2024-09-04 13:49:15 -0300

    cambios db

--- src/server/db.ts
+++ src/server/db.ts
@@ -14,13 +14,20 @@ interface Event {
     is_active: boolean;
 }
 
+type Gaf = 'PULGUITAS' | 'PREMINI' | 'MINI' | 'PRE INFANTIL' | 'INFANTIL' | 'JUVENIL' | 'MAYOR'
+type Gam = 'PULGUITAS' | 'MINI' | 'PRE INFANTIL' | 'INFANTIL' | 'CADETES' | 'JUVENILES' | 'JUNIOR' | 'SENIOR'
+
 interface Participant {
     id: number;
     first_name: string;
     last_name: string;
     dni: string;
+    birth: Date;
+    gender: 'M' | 'F';
     phone?: string;
     institution_name?: string;
+    level: 'ESCUELA 1' | 'ESCUELA 2' | 'NIVEL 1' | 'NIVEL 2' | 'NIVEL 3' | 'NIVEL 4' | 'NIVEL 5' | 'NIVEL 6' | 'NIVEL 7' | 'NIVEL 8' | 'NIVEL 9';
+    category: Gaf | Gam;
 }
 
 interface EventParticipant {
@@ -28,12 +35,26 @@ interface EventParticipant {
     event_id: number;
     participant_id: number;
     participant_institution_name?: string;
-    level: 'ESCUELA 1' | 'ESCUELA 2' | 'NIVEL 1' | 'NIVEL 2' | 'NIVEL 3' | 'NIVEL 4' | 'NIVEL 5' | 'NIVEL 6' | 'NIVEL 7' | 'NIVEL 8' | 'NIVEL 9';
-    category: 'PULGUITAS' | 'PREMINI' | 'MINI' | 'PRE INFANTIL' | 'INFANTIL' | 'JUVENIL' | 'MAYORES';
+    participant_level?: string;
+    participant_category?: string;
+}
+
+interface NoteGaf {
+    event_participant_id: number;
     salto_note: number;
     paralelas_note: number;
     viga_note: number;
     suelo_note: number;
+}
+
+interface NoteGam {
+    event_participant_id: number;
+    salto_note: number;
+    paralelas_note: number;
+    barra_fija_note: number;
+    suelo_note: number;
+    razones_note: number;
+    anillas_note: number;
 }
 
 const db = new Dexie('GymEventsDatabase') as Dexie & {
@@ -41,14 +62,18 @@ const db = new Dexie('GymEventsDatabase') as Dexie & {
     events: EntityTable<Event, 'id'>;
     participants: EntityTable<Participant, 'id'>;
     events_participants: EntityTable<EventParticipant, 'id'>;
+    notes_gaf: EntityTable<NoteGaf, 'event_participant_id'>;
+    notes_gam: EntityTable<NoteGam, 'event_participant_id'>;
 };
 
 db.version(1).stores({
     activation: 'id++, is_activated',
     events: 'id++, name, description, date, location, is_active',
-    participants: 'id++, first_name, last_name, dni, phone, institution_name',
-    events_participants: 'id++, event_id, participant_id, participant_institution_name, level, category, salto_note, paralelas_note, viga_note, suelo_note'
+    participants: 'id++, first_name, last_name, dni, birth, gender, phone, institution_name, level, category',
+    events_participants: 'id++, event_id, participant_id, participant_institution_name, participant_level, participant_category',
+    notes_gaf: 'event_participant_id++, salto_note, paralelas_note, viga_note, suelo_note',
+    notes_gam: 'event_participant_id++, salto_note, paralelas_note, barra_fija_note, suelo_note, razones_note, anillas_note'
 });
 
-export type { Activation, Event, Participant, EventParticipant };
+export type { Activation, Event, Participant, EventParticipant, NoteGaf, NoteGam };
 export { db };
